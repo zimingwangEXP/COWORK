99dcab66f00190e039043bba608aed929ec656fa
hsnowsunny806 <33408492+hsnowsunny806@users.noreply.github.com>
2018-05-30 02:09:59 +0000

    Update test.java

--- src/test.java
+++ src/test.java
@@ -11,12 +11,7 @@ public class test {
 
     public static void main(String[] args) throws SQLException {
 
-       // System.out.println(new dao().getUser("120"));
-        //new dao().getMessage("120");
-//		List<message> li = new dao().getMessage("120");
-//		for(message m:li){
-//			System.out.println(m);
-//		}
+      
          ArrayList<failrelation> li = new dao().getBlack("123");
          for(int i = 0; i <li.size();i++){
              failrelation r = new failrelation();
